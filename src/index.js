58dc04a99c134dbd5c98e582be9485091c57ab38
Ajarnshilin <ajarnshilin@gmail.com>
2021-01-07 11:50:59 +0700

    Add provider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { ListProvider } from './context/ListContextV2'
+import { Provider, provider } from 'react-redux'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ListProvider>
-    <App />
-    </ListProvider>
+    <Provider>
+      <ListProvider>
+        <App />
+      </ListProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
