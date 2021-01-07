fe5090d3c59256d97242743d7380a87f6507be9b
DogeCnx <thiraphat.re0096@gmail.com>
2021-01-07 15:39:21 +0700

    ADD all hell

---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux'
+import { storePlaylists }  from './storePlaylists'
+export default combineReducers({
+    storePlaylists
+})
---
+++ src/reducers/storePlaylists.js
@@ -0,0 +1,15 @@
+export default function storePlaylists(state = [], action) {
+    switch (action.type) {
+      case 'ADDPLAYLIST':
+        return [...state, { name: action.name, song : action.song }]
+      case 'EDITSONG':
+        let newState = Object.assign([], state)
+        let stateUpdate = newState.find(element => element.name === action.name) 
+        stateUpdate.song = action.song
+        const index = newState.indexOf(element => element.name === action.name)
+        newState[index] = stateUpdate  
+        return [...newState]
+      default:
+        return state
+    }
+  }
